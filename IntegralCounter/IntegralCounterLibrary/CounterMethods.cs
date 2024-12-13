5c70c9fb972d8184c3ce66e0d523e1614d10eb7a
Garik <bbanan570@gmail.com>
2024-12-13 21:00:40 +0300

    Wip wpf

---
+++ IntegralCounter/IntegralCounterLibrary/CounterMethods.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegralCounterLibrary
+{
+    public class CounterMethods
+    {
+        Func<double, double> ParabolaGetY = (x) => x * x;
+        double tolerance = 0.000001;
+
+        public double Tolerance { get => tolerance; set => tolerance = value; }
+        public Func<double, double> ParabolaGetY1 { get => ParabolaGetY; set => ParabolaGetY = value; }
+
+        public CounterMethods() { }
+
+        public double Integrate(Func<double, double> func, double a, double b, double tolerance)
+        {
+            double n = 1;
+            double previousArea = 0;
+
+            while (true)
+            {
+                double width = (b - a) / n;
+                double area = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    double x = a + i * width;
+                    area += func(x) * width;
+                }
+
+                if (area != previousArea)
+                {
+                    if (Math.Abs(area - previousArea) < tolerance)
+                    {
+                        break;
+                    }
+                }
+
+                previousArea = area;
+                n *= 2;
+
+            }
+
+            return previousArea;
+        }
+
+        public double IntegrateWithSegments(double a, double b, int segments)
+        {
+            double segmentSize = (b - a) / segments;
+            double areaSum = 0;
+
+            for (int i = 0; i < segments; i++)
+            {
+                areaSum += Integrate(ParabolaGetY1, i * segmentSize, i * segmentSize + segmentSize, Tolerance);
+            }
+
+            return areaSum;
+        }
+        public double IntegrateThreads(double a, double b, int segments)
+        {
+            double segmentSize = (b - a) / segments;
+            double areaSum = 0;
+            object locker = new object();
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < segments; i++)
+            {
+                int tempIndex = i;
+                Thread thread = new Thread(() =>
+                {
+                    double tempSum = Integrate(ParabolaGetY1, tempIndex * segmentSize, tempIndex * segmentSize + segmentSize, Tolerance);
+                    lock (locker)
+                    {
+                        areaSum += tempSum;
+                    }
+                });
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            return areaSum;
+        }
+
+        public double IntegrateParallel(double a, double b, int segments)
+        {
+            double segmentSize = (b - a) / segments;
+            double areaSum = 0;
+            object locker = new object();
+
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 0; i < segments; i++)
+            {
+                int tempIndex = i;
+                tasks.Add(Task.Run(() =>
+                {
+                    double tempSum = Integrate(ParabolaGetY1, tempIndex * segmentSize, tempIndex * segmentSize + segmentSize, Tolerance);
+                    lock (locker)
+                    {
+                        areaSum += tempSum;
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            return areaSum;
+        }
+
+
+    }
+}
--- IntegralCounter/IntegralCounterWPF/MainWindow.xaml.cs
+++ IntegralCounter/IntegralCounterWPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using LiveCharts;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IntegralCounterLibrary;
+using System.Diagnostics;
 
 namespace IntegralCounterWPF
 {
@@ -16,9 +21,63 @@ namespace IntegralCounterWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int ProcCount = Environment.ProcessorCount;
+        private CounterMethods counterMethods;
+        public SeriesCollection SeriesCollection { get; set; }
+        Stopwatch Stopwatch;
+        double starterTolerance = 0.1;
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            counterMethods = new CounterMethods();
+            Stopwatch = new Stopwatch();
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Обычный",
+                    Values = new ChartValues<ObservablePoint>{ }
+                },
+                new LineSeries
+                {
+                    Title = "Сегментный",
+                    Values = new ChartValues<ObservablePoint>{ }
+                },
+                new LineSeries
+                {
+                    Title = "Thread",
+                    Values = new ChartValues<ObservablePoint>{ }
+                },
+                new LineSeries
+                {
+                    Title = "Task",
+                    Values = new ChartValues<ObservablePoint>{ }
+                }
+            };
+        }
+        private void initializeProcessors()
+        {
+            procCountComboBox.Items.Clear();
+            for (int i = 1; i < ProcCount + 1; i++)
+            {
+                procCountComboBox.Items.Add(i);
+            }
+            procCountComboBox.SelectedItem = 1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SeriesCollection[0].Values.Clear();
+            SeriesCollection[1].Values.Clear();
+            SeriesCollection[2].Values.Clear();
+            SeriesCollection[3].Values.Clear();
+
+            Stopwatch.Start();
+            double start = Convert.ToDouble(LowerBorder.Text);
+            double end = Convert.ToDouble(UpperBorder.Text);
+            counterMethods.Integrate(counterMethods.ParabolaGetY1, start, end, starterTolerance);
+
         }
     }
 }
